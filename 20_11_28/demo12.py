12b40aef26b869b98613aabce35c23c9e03bef3f
976898643@qq.com <976898643@qq.com>
2020-11-28 23:31:25 +0800

    python 对象

---
+++ 20_11_28/demo12.py
@@ -0,0 +1,22 @@
+class Turtle:
+    color = 'green'
+    weight = 10
+    legs = 4
+    shell = True
+    mouth = 'dazui'
+
+    def climb(self):
+        print('1')
+    
+    def run(self):
+        print('2')
+    
+    def bite(self):
+        print('3')
+    
+    def eat(self):
+        print('4')
+
+tt = Turtle()
+print(tt.climb())
+
