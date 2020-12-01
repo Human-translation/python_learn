006c8c4c13665d5f2b7f890cc87a664053f4be9a
976898643@qq.com <976898643@qq.com>
2020-12-01 23:23:04 +0800

    python日常练习

---
+++ learn/20_12_1/lianxi.py
@@ -0,0 +1,57 @@
+num = 1
+
+for i in range(num):
+    if i%2:
+        print(i,end=' ')
+
+
+def paiming(num):
+    if num > 90:
+        print("A")
+    elif num > 80:
+        print("B")
+    elif num >70:
+        print("C")
+    elif num > 60:
+        print("D")
+
+paiming(75)
+
+x=10
+y=20
+z=30
+small = x if (x < y and x < z) else (y if y < z else z)
+
+
+def fun():
+    count = 3
+    password = "nihao"
+    while count:
+        str = input()
+        if str == password:
+            print("true")
+            break
+        if "*" in str:
+            print("No")
+            continue
+        else:
+            print("False")
+        count -= 1
+
+def fun2():
+    for number in range(100,1000):
+        baiwei = number // 100
+        shiwei = number % 100 // 10
+        gewei = number % 10
+        if baiwei ** 3 + shiwei ** 3 + gewei ** 3 == number:
+            print(number,end=" ")
+
+arr = 10000
+def fun3():
+    print(arr)
+fun3()
+
+
+
+
+
