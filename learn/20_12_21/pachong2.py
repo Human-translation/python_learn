46b6ae1d1fba6874fb926be1481909ac12896e30
976898643@qq.com <976898643@qq.com>
2020-12-21 23:12:29 +0800

    python 爬虫

---
+++ learn/20_12_21/pachong2.py
@@ -0,0 +1,22 @@
+import urllib.request
+import random
+
+
+url = "http://www.whatismyip.com.tw"
+
+iplist = ['157.7.143.36:1080','104.149.134.114:3128','157.7.197.239:1080','170.0.54.232:8080']
+i = random.randint(0,len(iplist)-1)
+
+
+proxy_support = urllib.request.ProxyHandler({"http":iplist[i]})
+
+opener = urllib.request.build_opener(proxy_support)
+opener.addheaders = [("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")]
+
+
+urllib.request.install_opener(opener)
+
+response = urllib.request.urlopen(url)
+html = response.read().decode('utf-8')
+
+print(html)
